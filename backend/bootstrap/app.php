cc5a42d805db7a4d4823b3a33fd9363396a3137e
saddamxii <assiane.fahd@gmail.com>
2026-08-15 14:11:31 +0100

    feat(security): register access-token middleware

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'role' => App\Http\Middleware\RoleMiddleware::class,
             'permission' => App\Http\Middleware\PermissionMiddleware::class,
+            'access.token' => App\Http\Middleware\EnsureAccessToken::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
