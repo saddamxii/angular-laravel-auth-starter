7bfad09c7717852c19882bfa542febc11d3e655a
saddamxii <assiane.fahd@gmail.com>
2026-08-15 14:12:20 +0100

    fix(test): bootstrap Laravel application in tests

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -2,9 +2,13 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    // Base test case for feature and integration tests.
+    public function createApplication(): Application
+    {
+        return require __DIR__.'/../bootstrap/app.php';
+    }
 }
